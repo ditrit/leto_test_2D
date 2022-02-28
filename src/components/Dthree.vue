1a8e51c7ac5c3a1fcc95a4521e44162a8179dda7
Alexis <alexis.nvu@gmail.com>
2022-02-28 14:50:12 +0100

    Dthree component issue resolved

--- src/components/Dthree.vue
+++ src/components/Dthree.vue
@@ -1,37 +1,22 @@
 <template>
-  <canvas id="figure" width="350" height="200"></canvas>
+  <div style="display: flex">
+    <div id="container" style="height: 100ch; width: 150ch"></div>
+  </div>
 </template>
 
 <script>
-const IsoBlock = require("src/main.js");
-
+//const Cytoscape = require("cytoscape");
 export default {
   name: "Dthree",
 
   data() {
     return {};
   },
-  mounted() {
-    var blocks = [
-      new IsoBlock.Block(
-        { x: 1, y: 3, z: 0 },
-        { x: 2, y: 2, z: 2.5 },
-        { light: "#8ae234", medium: "#73d216", dark: "#4e9a06" }
-      ),
-      new IsoBlock.Block(
-        { x: 2, y: 2, z: 0 },
-        { x: 1, y: 1, z: 1.5 },
-        IsoBlock.colors.red
-      ),
-      new IsoBlock.Block(
-        { x: 3, y: 1, z: 0 },
-        { x: 1, y: 4, z: 1 },
-        IsoBlock.colors.blue
-      ),
-    ];
-
-    IsoBlock.makeFigure({ canvas: "figure", blocks: blocks });
+  mounted() {},
+  methods: {
+    make: function () {
+      return;
+    },
   },
-  methods: {},
 };
 </script>
