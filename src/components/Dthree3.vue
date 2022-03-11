76c3ac167f3149de63f3459301bbc48d08a33153
Alexis <alexis.nvu@gmail.com>
2022-03-11 09:14:37 +0100

    drawer and drag/drop with still some issues

--- src/components/Dthree3.vue
+++ src/components/Dthree3.vue
@@ -1,9 +1,20 @@
 <template>
+  <!-- <q-drawer
+    v-model="drawerLeft"
+    show-if-above
+    :width="200"
+    :breakpoint="700"
+    style="background-color: white"
+  >
+    <q-item-label header> Models </q-item-label>
+    <q-scroll-area class="fit">
+      <div id="drawerContent"></div>
+    </q-scroll-area> </q-drawer
+  >-->
   <div id="my_dataviz"></div>
 </template>
 
 <script>
-import store from "src/store";
 const d3 = require("d3");
 
 export default {
@@ -12,31 +23,57 @@ export default {
   data() {
     return {
       arrowOutData: [
-        { x: 490, y: 220 },
-        { x: 520, y: 220 },
-        { x: 530, y: 230 },
-        { x: 520, y: 240 },
-        { x: 490, y: 240 },
-        { x: 490, y: 220 },
+        { x: 275, y: 220 },
+        { x: 320, y: 220 },
+        { x: 330, y: 230 },
+        { x: 320, y: 240 },
+        { x: 275, y: 240 },
+        { x: 275, y: 220 },
       ],
       arrowInData: [
-        { x: 470, y: 270 },
-        { x: 480, y: 260 },
-        { x: 510, y: 260 },
-        { x: 510, y: 280 },
-        { x: 480, y: 280 },
-        { x: 470, y: 270 },
+        { x: 275, y: 270 },
+        { x: 285, y: 260 },
+        { x: 330, y: 260 },
+        { x: 330, y: 280 },
+        { x: 285, y: 280 },
+        { x: 275, y: 270 },
+      ],
+      lilarrowOutData: [
+        { x: 175, y: 120 },
+        { x: 220, y: 120 },
+        { x: 230, y: 130 },
+        { x: 220, y: 140 },
+        { x: 175, y: 140 },
+        { x: 175, y: 120 },
+      ],
+      lilarrowInData: [
+        { x: 175, y: 170 },
+        { x: 185, y: 160 },
+        { x: 230, y: 160 },
+        { x: 230, y: 180 },
+        { x: 185, y: 180 },
+        { x: 175, y: 170 },
       ],
     };
   },
   mounted() {
+    //Create SVG element on the center page
     var svg = d3
       .select("#my_dataviz")
       .append("svg")
-      .attr("id", 0)
+      .attr("id", "svg0")
       .attr("width", 1000)
       .attr("height", 1000);
 
+    //Create SVG element in the drawer
+    var modelsWorkshop = d3
+      .select("#drawerContent")
+      .append("svg")
+      .attr("id", "svg1")
+      .attr("width", 200)
+      .attr("height", 500);
+
+    // Define drag and drop functions ----------------------------------------
     function dragstarted() {
       d3.select(this).attr("stroke", "black");
     }
@@ -46,9 +83,33 @@ export default {
         d3.select(this)
           .raise()
           .attr("transform", "translate(" + [event.x, event.y] + ")");
+        event.sourceEvent.stopPropagation();
       } else {
         d3.select(this).raise().attr("x", event.x).attr("y", event.y);
+        event.sourceEvent.stopPropagation();
       }
+
+      var groups = d3.selectAll("g");
+
+      groups.each(function (groups, i) {
+        var groupRect = this.childNodes[0].getBoundingClientRect();
+        //console.log(groupRect);
+        if (
+          event.x > groupRect.x - 30 &&
+          event.x < groupRect.x + groupRect.width - 30 &&
+          event.y > groupRect.y + 10 &&
+          event.y < groupRect.y + groupRect.height + 10
+        ) {
+          console.log("dragover" + this.getAttribute("id"));
+          //d3.select(this.childNodes[0]).style("stroke-width", 5);
+        } else {
+          /*var groupArray = d3.selectAll("g");
+          groupArray.forEach((element) => {
+            this.childNodes[0].style("stroke-width", 2);
+          });
+          */
+        }
+      });
     }
 
     function dragended(event, d) {
@@ -61,29 +122,36 @@ export default {
 
       if (this.getAttribute("class") != "main") {
         if (
-          event.x > parentRect.x &&
-          event.x < parentRect.x + parentRect.width &&
-          event.y > parentRect.y &&
-          event.y < parentRect.y + parentRect.height &&
+          event.x > parentRect.x - 30 &&
+          event.x < parentRect.x + parentRect.width - 30 &&
+          event.y > parentRect.y + 10 &&
+          event.y < parentRect.y + parentRect.height + 10 &&
           parent.tagName != "svg"
         ) {
           console.log("in his group");
         } else {
           console.log("out of his group");
+          console.log(parent.getAttribute("id"));
+
           parent.removeChild(this);
+
           var groups = d3.selectAll("g");
 
           groups.each(function (groups, i) {
-            var groupRect = this.getBoundingClientRect();
-            console.log(groupRect);
+            var groupRect = this.childNodes[0].getBoundingClientRect();
+            //console.log(groupRect);
             if (
-              event.x > groupRect.x &&
-              event.x < groupRect.x + groupRect.width &&
-              event.y > groupRect.y &&
-              event.y < groupRect.y + groupRect.height
+              event.x > groupRect.x - 30 &&
+              event.x < groupRect.x + groupRect.width - 30 &&
+              event.y > groupRect.y + 10 &&
+              event.y < groupRect.y + groupRect.height + 10
             ) {
               console.log("in a new group");
+              console.log(this.getAttribute("id"));
+
               var clone = component.cloneNode(true);
+              d3.select(clone).attr("x", groupRect.x).attr("y", groupRect.y);
+
               document
                 .getElementById(this.getAttribute("id"))
                 .appendChild(clone);
@@ -94,17 +162,31 @@ export default {
           });
           if (!reDrawn) {
             console.log("on svg");
+
             var clone = component.cloneNode(true);
-            document.getElementById("0").appendChild(clone);
+            document.getElementById("svg0").appendChild(clone);
             d3.select(clone).call(drag).on("click", click);
           }
         }
       }
     }
+    //End of drag and drop functions -------------------------------------------
 
+    //Click event
     function click(event, d) {
       if (event.defaultPrevented) return; // dragged
       d3.select(this).transition().attr("fill", "black");
+    }
+
+    function click_model(event, d) {
+      console.log("clicked");
+      d3.select(this).transition().attr("fill", "black");
+      var clone = this.cloneNode(true);
+      document.getElementById("svg0").appendChild(clone);
+      d3.select(clone.childNodes[0])
+        .attr("width", clone.childNodes[0].getAttribute("width") * 2)
+        .attr("height", clone.childNodes[0].getAttribute("height") * 2);
+      d3.select(clone).attr("id", Date.now()).call(drag).on("click", click);
     }
 
     const drag = d3
@@ -113,7 +195,9 @@ export default {
       .on("drag", dragged)
       .on("end", dragended);
 
-    // prepare a helper function
+    //Create some svg items
+
+    // Helper func to draw the arrows
     var lineFunc = d3
       .line()
       .x(function (d) {
@@ -123,26 +207,28 @@ export default {
         return d.y;
       });
 
-    var group = svg
+    // Draw some svg items -------------------------------------------------------
+
+    var group = modelsWorkshop
       .append("g")
       .attr("class", "main")
-      .attr("id", 1)
-      .call(drag)
-      .on("click", click);
+      .attr("id", "model1")
+      .attr("transform", "translate(0, 0)")
+      .on("click", click_model);
 
     var box = group
       .append("rect")
-      .attr("width", 500)
-      .attr("height", 500)
-      .attr("x", 0)
-      .attr("y", 0)
+      .attr("width", 150)
+      .attr("height", 250)
+      .attr("x", 10)
+      .attr("y", 150)
       .style("stroke", "black")
       .style("fill", "yellow");
 
     var title = group
       .append("text")
-      .attr("x", 10)
-      .attr("y", 5)
+      .attr("x", 50)
+      .attr("y", 155)
       .attr("dominant-baseline", "hanging")
       .text("DB Server");
 
@@ -159,32 +245,141 @@ export default {
       .attr("d", lineFunc(this.arrowInData))
       .attr("stroke", "black")
       .attr("fill", "red");
+    group
+      .append("svg:image")
+      .attr("x", 30)
+      .attr("y", 180)
+      .attr("width", 50)
+      .attr("height", 50)
+      .attr(
+        "xlink:href",
+        "https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-server-miscellaneous-kiranshastry-lineal-kiranshastry.png"
+      );
 
-    var lilgroup = group
-      .append("g")
-      .attr("id", 2)
-      .call(drag)
-      .on("click", click);
-
-    var lilbox = lilgroup
+    /*var handle = group
       .append("rect")
-      .attr("width", 200)
-      .attr("height", 200)
-      .attr("x", 50)
-      .attr("y", 50)
-      .attr("id", 12)
-      .attr("parentId", 1)
+      .data([
+        {
+          // Position of the rectangle
+          x: 0,
+          y: 0,
+        },
+      ])
+      .attr("class", "draghandle")
+      .attr("x", 5)
+      .attr("y", 145)
+      .attr("width", 20)
+      .attr("height", 20)
+      .style("fill", "blue")
+      .style("opacity", 1)
+      .attr("cursor", "move")
+      .call(
+        d3.drag().on("drag", function (d) {
+          console.log("yep");
+          d.x += d3.event.dx;
+          d.y += d3.event.dy;
+
+          // Move handle rect
+          d3.select(this)
+            .attr("x", function (d) {
+              return d.x;
+            })
+            .attr("y", function (d) {
+              return d.y;
+            });
+
+          // Move Group
+          d3.select("#group1").attr(
+            "transform",
+            "translate(" + [d.x, d.y] + ")"
+          );
+        })
+      );*/
+
+    var grp2 = modelsWorkshop
+      .append("g")
+      .attr("id", "model2")
+      .on("click", click_model);
+
+    grp2
+      .append("rect")
+      .attr("width", 100)
+      .attr("height", 100)
+      .attr("x", 10)
+      .attr("y", 10)
       .style("stroke", "black")
       .style("fill", "orange");
 
-    var liltitle = lilgroup
+    grp2
       .append("text")
-      .attr("x", 60)
-      .attr("y", 60)
+      .attr("x", 30)
+      .attr("y", 20)
       .attr("dominant-baseline", "hanging")
       .text("Data Base");
 
-    //group.call(drag).on("click", click);
+    // Add the path using this helper function
+    var arrowOut = grp2
+      .append("path")
+      .attr("d", lineFunc(this.lilarrowOutData))
+      .attr("stroke", "black")
+      .attr("fill", "blue");
+
+    // Add the path using this helper function
+    var arrowIn = grp2
+      .append("path")
+      .attr("d", lineFunc(this.lilarrowInData))
+      .attr("stroke", "black")
+      .attr("fill", "red");
+
+    grp2
+      .append("svg:image")
+      .attr("x", 30)
+      .attr("y", 50)
+      .attr("width", 40)
+      .attr("height", 40)
+      .attr("xlink:href", "https://img.icons8.com/ios/50/000000/database.png");
+
+    /*var lilhandle = grp2
+      .append("rect")
+      .data([
+        {
+          // Position of the rectangle
+          x: 0,
+          y: 0,
+        },
+      ])
+      .attr("class", "draghandle")
+      .attr("x", 5)
+      .attr("y", 5)
+      .attr("width", 20)
+      .attr("height", 20)
+      .style("fill", "blue")
+      .style("opacity", 1)
+      .attr("cursor", "move")
+      .call(
+        d3.drag().on("drag", function (d) {
+          console.log("yep");
+          d.x += d3.event.dx;
+          d.y += d3.event.dy;
+
+          // Move handle rect
+          d3.select(this)
+            .attr("x", function (d) {
+              return d.x;
+            })
+            .attr("y", function (d) {
+              return d.y;
+            });
+
+          // Move Group
+          d3.select("#model1").attr(
+            "transform",
+            "translate(" + [d.x, d.y] + ")"
+          );
+        })
+      );*/
+
+    //----------------------------------------------------------------------------
   },
   methods: {},
 };
